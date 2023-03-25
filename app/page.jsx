e8114222d3a747b6890181530ef4eb8d343f0227
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-03-25 14:08:50 -0400

    Initial setup with Tailwind

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+const Page = () => {
+    return (
+        <section className="py-20">
+            <div className="container">
+                <h1 className="text-3xl font-bold">Home page</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Page;
